c7e346f454ea85feaca0e28044652673b5e1bdf6
Hao Truong Vi <truongvihao24@gmail.com>
2021-12-31 00:34:07 +0700

    feat: setup admin template

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    //
+    public function addcategory()
+    {
+        return view('admin.addcategory');
+    }
+
+    public function categories()
+    {
+        return view('admin.categories');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SliderController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +18,21 @@ use App\Http\Controllers\ClientController;
 |
 */
 
+Route::get('/admin', [AdminController::class, 'admin']);
+Route::get('/addcategory', [CategoryController::class, 'addcategory']);
+Route::get('/categories', [CategoryController::class, 'categories']);
+Route::get('/addslider', [SliderController::class, 'addslider']);
+Route::get('/sliders', [SliderController::class, 'sliders']);
+Route::get('/addproduct', [ProductController::class, 'addproduct']);
+Route::get('/products', [ProductController::class, 'products']);
+
 Route::get('/', [ClientController::class, 'home']);
 Route::get('/shop', [ClientController::class, 'shop']);
 Route::get('/cart', [ClientController::class, 'cart']);
 Route::get('/checkout', [ClientController::class, 'checkout']);
 Route::get('/login', [ClientController::class, 'login']);
 Route::get('/signup', [ClientController::class, 'signup']);
+Route::get('/orders', [ClientController::class, 'orders']);
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
